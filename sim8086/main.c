77b7553da5e5002f30d2d2bedb0cdfef3b9c6fc7
Joseph Hines <jthines3@gmail.com>
2023-04-29 18:06:48 -0400

    Listing 39 complete

--- sim8086/main.c
+++ sim8086/main.c
@@ -40,15 +40,30 @@ typedef struct sim_state_t {
 // ============================================================================
 // Constants
 // ============================================================================
-// Table 4-9
-const char **REG_TABLE[2] = {
+
+// Table 4-9 (MOD = 11)
+const char **REGISTER_MODE_TABLE[2] = {
 	(const char*[8]){"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"}, // w = 0
 	(const char*[8]){"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"}, // w = 1
+};
+
+const char *EFFECTIVE_ADDRESS_CALCULATION_TABLE[8] = {
+	"bx + si",
+	"bx + di",
+	"bp + si",
+	"bp + di",
+	"si",
+	"di",
+	"bp",
+	"bx",
 };
 
 // ============================================================================
 // Utils
 // ============================================================================
+
+// Read a byte from the file, exiting if it is EOF. Otherwise, return the byte
+// as an unsigned 8-bit integer
 uint8_t read_byte(FILE *fp) {
 	int b = fgetc(fp);
 	if (b == EOF) {
@@ -61,7 +76,7 @@ uint8_t read_byte(FILE *fp) {
 // ============================================================================
 // Instruction handlers
 // ============================================================================
-void handle_mov_0(sim_state_t *state, op_t *op) {
+void mov_rm2rm(sim_state_t *state, op_t *op) {
 	state->b1 = read_byte(state->fp);
 
 	uint8_t d = (state->b0 >> 1) & 1;
@@ -71,11 +86,85 @@ void handle_mov_0(sim_state_t *state, op_t *op) {
 	uint8_t reg = (state->b1 >> 3) & 0b111;
 	uint8_t rm = state->b1 & 0b111;
 
-	if (d) {
-		printf("mov %s, %s\n", REG_TABLE[w][reg], REG_TABLE[w][rm]);
-	} else {
-		printf("mov %s, %s\n", REG_TABLE[w][rm], REG_TABLE[w][reg]);
+	switch (mod) {
+		case 0b11:
+			// Register mode
+			if (d) {
+				printf("mov %s, %s\n", REGISTER_MODE_TABLE[w][reg], REGISTER_MODE_TABLE[w][rm]);
+			} else {
+				printf("mov %s, %s\n", REGISTER_MODE_TABLE[w][rm], REGISTER_MODE_TABLE[w][reg]);
+			}
+			break;
+		case 0b10:
+			// Memory mode, 16-bit displacement
+			state->b2 = read_byte(state->fp);
+			state->b3 = read_byte(state->fp);
+			uint16_t v = (state->b3 << 8) | (state->b2);
+			if (d) {
+				printf("mov %s, [%s + %d]\n", REGISTER_MODE_TABLE[w][reg], EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], v);
+			} else {
+				printf("mov [%s + %d], %s\n", EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], v, REGISTER_MODE_TABLE[w][reg]);
+			}
+			break;
+		case 0b01:
+			// Memory mode, 8-bit displacement
+			state->b2 = read_byte(state->fp);
+			if (d) {
+				printf("mov %s, [%s + %d]\n", REGISTER_MODE_TABLE[w][reg], EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], state->b2);
+			} else {
+				printf("mov [%s + %d], %s\n", EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], state->b2, REGISTER_MODE_TABLE[w][reg]);
+			}
+			break;
+		case 0b00:
+			// Memory mode, no displacement (except when rm = 0b110, then 16-bit)
+			if (rm == 0b110) {
+				// Direct address, 16-bit
+				state->b2 = read_byte(state->fp);
+				state->b3 = read_byte(state->fp);
+				uint16_t v = (state->b3 << 8) | (state->b2);
+				if (d) {
+					printf("mov %s, [%d]\n", REGISTER_MODE_TABLE[w][reg], v);
+				} else {
+					printf("mov [%d], %s\n", v, REGISTER_MODE_TABLE[w][reg]);
+				}
+			} else {
+				// Normal memory mode, no displacement
+				if (d) {
+					printf("mov %s, [%s]\n", REGISTER_MODE_TABLE[w][reg], EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm]);
+				} else {
+					printf("mov [%s], %s\n", EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], REGISTER_MODE_TABLE[w][reg]);
+				}
+			}
+			break;
+		default:
+			break;
 	}
+}
+
+void mov_i2rm(sim_state_t *state, op_t *op) {
+	printf("move_i2rm\n");
+}
+
+void mov_i2r(sim_state_t *state, op_t *op) {
+	uint8_t w = (state->b0 >> 3) & 1;
+	uint8_t reg = (state->b0) & 0b111;
+
+	if (w) {
+		state->b1 = read_byte(state->fp);
+		state->b2 = read_byte(state->fp);
+		uint16_t v = (state->b2 << 8) | (state->b1);
+		printf("mov %s, %d \n", REGISTER_MODE_TABLE[w][reg], v);
+	} else {
+		state->b1 = read_byte(state->fp);
+		printf("mov %s, %d \n", REGISTER_MODE_TABLE[w][reg], state->b1);
+	}
+}
+
+void mov_m2a(sim_state_t *state, op_t *op) {
+	printf("move_m2a\n");
+}
+void mov_a2m(sim_state_t *state, op_t *op) {
+	printf("move_a2m\n");
 }
 
 
@@ -83,8 +172,13 @@ void handle_mov_0(sim_state_t *state, op_t *op) {
 // Operation list
 // ============================================================================
 const op_t OPS[] = {
-	(op_t) { .opcode_len = 6, .opcode = 0b100010, .impl = handle_mov_0 },
+	(op_t) { .opcode_len = 6,  .opcode = 0b100010,   .impl = mov_rm2rm          },
+	(op_t) { .opcode_len = 7,  .opcode = 0b1100011,  .impl = mov_i2rm           },
+	(op_t) { .opcode_len = 4,  .opcode = 0b1011,     .impl = mov_i2r            },
+	(op_t) { .opcode_len = 7,  .opcode = 0b1010000,  .impl = mov_m2a            },
+	(op_t) { .opcode_len = 7,  .opcode = 0b1010001,  .impl = mov_a2m            },
 };
+const int NUM_OPS = sizeof(OPS)/sizeof(op_t);
 
 // ============================================================================
 // Entrypoint
@@ -94,21 +188,25 @@ int main(int argc, char *argv[]) {
 		printf("Must provide machine code filename as sole argument\n");
 		return 1;
 	}
-	sim_state_t state = {0};
 
+	// Init state and open input file
+	sim_state_t state = {0};
 	state.fp = fopen(argv[1], "rb");
 	if (state.fp == NULL) {
 		printf("Could not open file {%s}\n", argv[1]);
 		return 2;
 	}
 
+	// Write header
 	printf("; %s:\nbits 16\n\n", argv[1]);
 
+	// Read bytes while there are bytes left and we haven't hit an error
 	while (1) {
 		state.b0 = read_byte(state.fp);
 
+		// Check if current byte starts a known op
 		uint8_t found = 0;
-		for (int i = 0; i < sizeof(OPS)/sizeof(op_t); i++) {
+		for (int i = 0; i < NUM_OPS; i++) {
 			op_t op = OPS[i];
 			if ((state.b0 >> (8-op.opcode_len)) == op.opcode) {
 				op.impl(&state, &op);
@@ -119,6 +217,7 @@ int main(int argc, char *argv[]) {
 
 		if (!found) {
 			printf("Unknown op from byte=%d\n", state.b0);
+			fclose(state.fp);
 			return 100;
 		}
 	}
