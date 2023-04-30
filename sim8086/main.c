a5eb155d7ed5e41107910baac683d8b2581e9fba
Joseph Hines <jthines3@gmail.com>
2023-04-30 09:56:01 -0400

    Listing 40

--- sim8086/main.c
+++ sim8086/main.c
@@ -15,27 +15,24 @@
 // Defines
 // ============================================================================
 
+#define REGISTER_MODE 3
+#define MEMORY_MODE_16 2
+#define MEMORY_MODE_8 1
+#define MEMORY_MODE_0 0
+
 // ============================================================================
 // Structs/Types
 // ============================================================================
-typedef struct op_t op_t;
-typedef struct sim_state_t sim_state_t;
 
-typedef struct op_t {
+struct sim_state_t {
+	FILE *fp; // Open file, where bytes are read from
+	uint8_t op_byte;
+};
+struct op_t {
 	uint8_t opcode_len;
 	uint8_t opcode;
-	void (*impl)(sim_state_t *state, op_t *op);
-} op_t;
-
-typedef struct sim_state_t {
-	FILE *fp; // Open file, where bytes are read from
-	uint8_t b0; // Relevant bytes for current op, most only use the first few
-	uint8_t b1;
-	uint8_t b2;
-	uint8_t b3;
-	uint8_t b4;
-	uint8_t b5;
-} sim_state_t;
+	void (*impl)(struct sim_state_t *state, struct op_t *op);
+};
 
 // ============================================================================
 // Constants
@@ -64,68 +61,72 @@ const char *EFFECTIVE_ADDRESS_CALCULATION_TABLE[8] = {
 
 // Read a byte from the file, exiting if it is EOF. Otherwise, return the byte
 // as an unsigned 8-bit integer
-uint8_t read_byte(FILE *fp) {
-	int b = fgetc(fp);
+uint8_t read_byte(struct sim_state_t *state) {
+	int b = fgetc(state->fp);
 	if (b == EOF) {
-		fclose(fp);
+		fclose(state->fp);
 		exit(0);
 	}
-	return b;
+	return (uint8_t)b;
+}
+
+// Read two bytes from the file, returning a signed word constructed from them.
+// The second byte contains the most significant bits.
+int16_t read_word(struct sim_state_t *state) {
+	uint8_t a = read_byte(state);
+	uint8_t b = read_byte(state);
+	return (uint16_t)(b << 8) | (uint16_t)(a);
 }
 
 // ============================================================================
 // Instruction handlers
 // ============================================================================
-void mov_rm2rm(sim_state_t *state, op_t *op) {
-	state->b1 = read_byte(state->fp);
+void mov_rm2rm(struct sim_state_t *state, struct op_t *op) {
+	uint8_t options_byte = read_byte(state);
 
-	uint8_t d = (state->b0 >> 1) & 1;
-	uint8_t w = state->b0 & 1;
+	uint8_t d = (state->op_byte >> 1) & 1;
+	uint8_t w = state->op_byte & 1;
 
-	uint8_t mod = (state->b1 >> 6) & 0b11;
-	uint8_t reg = (state->b1 >> 3) & 0b111;
-	uint8_t rm = state->b1 & 0b111;
+	uint8_t mod = (options_byte >> 6) & 3;
+	uint8_t reg = (options_byte >> 3) & 7;
+	uint8_t rm = options_byte & 7;
 
 	switch (mod) {
-		case 0b11:
-			// Register mode
+		case REGISTER_MODE:
 			if (d) {
 				printf("mov %s, %s\n", REGISTER_MODE_TABLE[w][reg], REGISTER_MODE_TABLE[w][rm]);
 			} else {
 				printf("mov %s, %s\n", REGISTER_MODE_TABLE[w][rm], REGISTER_MODE_TABLE[w][reg]);
 			}
 			break;
-		case 0b10:
-			// Memory mode, 16-bit displacement
-			state->b2 = read_byte(state->fp);
-			state->b3 = read_byte(state->fp);
-			uint16_t v = (state->b3 << 8) | (state->b2);
-			if (d) {
-				printf("mov %s, [%s + %d]\n", REGISTER_MODE_TABLE[w][reg], EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], v);
-			} else {
-				printf("mov [%s + %d], %s\n", EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], v, REGISTER_MODE_TABLE[w][reg]);
-			}
-			break;
-		case 0b01:
-			// Memory mode, 8-bit displacement
-			state->b2 = read_byte(state->fp);
-			if (d) {
-				printf("mov %s, [%s + %d]\n", REGISTER_MODE_TABLE[w][reg], EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], state->b2);
-			} else {
-				printf("mov [%s + %d], %s\n", EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], state->b2, REGISTER_MODE_TABLE[w][reg]);
-			}
-			break;
-		case 0b00:
-			// Memory mode, no displacement (except when rm = 0b110, then 16-bit)
-			if (rm == 0b110) {
-				// Direct address, 16-bit
-				state->b2 = read_byte(state->fp);
-				state->b3 = read_byte(state->fp);
-				uint16_t v = (state->b3 << 8) | (state->b2);
+		case MEMORY_MODE_16:
+			{
+				int16_t displacement = read_word(state);
 				if (d) {
-					printf("mov %s, [%d]\n", REGISTER_MODE_TABLE[w][reg], v);
+					printf("mov %s, [%s + %d]\n", REGISTER_MODE_TABLE[w][reg], EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], displacement);
 				} else {
-					printf("mov [%d], %s\n", v, REGISTER_MODE_TABLE[w][reg]);
+					printf("mov [%s + %d], %s\n", EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], displacement, REGISTER_MODE_TABLE[w][reg]);
+				}
+				break;
+			}
+		case MEMORY_MODE_8:
+			{
+				int8_t displacement = read_byte(state);
+				if (d) {
+					printf("mov %s, [%s + %d]\n", REGISTER_MODE_TABLE[w][reg], EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], displacement);
+				} else {
+					printf("mov [%s + %d], %s\n", EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], displacement, REGISTER_MODE_TABLE[w][reg]);
+				}
+				break;
+			}
+		case MEMORY_MODE_0:
+			if (rm == 6) {
+				// Direct address, 16-bit
+				int16_t displacement = read_word(state);
+				if (d) {
+					printf("mov %s, [%d]\n", REGISTER_MODE_TABLE[w][reg], displacement);
+				} else {
+					printf("mov [%d], %s\n", displacement, REGISTER_MODE_TABLE[w][reg]);
 				}
 			} else {
 				// Normal memory mode, no displacement
@@ -141,56 +142,131 @@ void mov_rm2rm(sim_state_t *state, op_t *op) {
 	}
 }
 
-void mov_i2rm(sim_state_t *state, op_t *op) {
-	printf("move_i2rm\n");
-}
+void mov_i2rm(struct sim_state_t *state, struct op_t *op) {
+	uint8_t w = state->op_byte & 1;
 
-void mov_i2r(sim_state_t *state, op_t *op) {
-	uint8_t w = (state->b0 >> 3) & 1;
-	uint8_t reg = (state->b0) & 0b111;
+	uint8_t options_byte = read_byte(state);
+	uint8_t mod = (options_byte>> 6) & 3;
+	uint8_t rm = options_byte & 7;
 
-	if (w) {
-		state->b1 = read_byte(state->fp);
-		state->b2 = read_byte(state->fp);
-		uint16_t v = (state->b2 << 8) | (state->b1);
-		printf("mov %s, %d \n", REGISTER_MODE_TABLE[w][reg], v);
-	} else {
-		state->b1 = read_byte(state->fp);
-		printf("mov %s, %d \n", REGISTER_MODE_TABLE[w][reg], state->b1);
+	switch (mod) {
+		case REGISTER_MODE:
+			if (w) {
+				int16_t data = read_word(state);
+				printf("mov %s, word %d\n", REGISTER_MODE_TABLE[w][rm], data);
+			} else {
+				int8_t data = read_byte(state);
+				printf("mov %s, byte %d\n", REGISTER_MODE_TABLE[w][rm], data);
+			}
+			break;
+		case MEMORY_MODE_16:
+			{
+				int16_t displacement = read_word(state);
+				if (w) {
+					int16_t data = read_word(state);
+					printf("mov [%s + %d], word %d\n", EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], displacement, data);
+				} else {
+					int8_t data = read_byte(state);
+					printf("mov [%s + %d], byte %d\n", EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], displacement, data);
+				}
+				break;
+			}
+		case MEMORY_MODE_8:
+			{
+				int8_t displacement = read_byte(state);
+				if (w) {
+					int16_t data = read_word(state);
+					printf("mov [%s + %d], word %d\n", EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], displacement, data);
+				} else {
+					int8_t data = read_byte(state);
+					printf("mov [%s + %d], byte %d\n", EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], displacement, data);
+				}
+				break;
+			}
+			break;
+		case MEMORY_MODE_0:
+			if (rm == 6) {
+				int16_t displacement = read_word(state);
+				if (w) {
+					int16_t data = read_word(state);
+					printf("mov [%d], word %d\n", displacement, data);
+				} else {
+					int8_t data = read_byte(state);
+					printf("mov [%d], byte %d\n", displacement, data);
+				}
+			} else {
+				if (w) {
+					int16_t data = read_word(state);
+					printf("mov [%s], word %d\n", EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], data);
+				} else {
+					int8_t data = read_byte(state);
+					printf("mov [%s], byte %d\n", EFFECTIVE_ADDRESS_CALCULATION_TABLE[rm], data);
+				}
+				break;
+			}
+		default:
+			break;
 	}
 }
 
-void mov_m2a(sim_state_t *state, op_t *op) {
-	printf("move_m2a\n");
-}
-void mov_a2m(sim_state_t *state, op_t *op) {
-	printf("move_a2m\n");
+void mov_i2r(struct sim_state_t *state, struct op_t *op) {
+	uint8_t w = (state->op_byte >> 3) & 1;
+	uint8_t reg = (state->op_byte) & 7;
+
+	if (w) {
+		int16_t data = read_word(state);
+		printf("mov %s, word %d \n", REGISTER_MODE_TABLE[w][reg], data);
+	} else {
+		int8_t data = read_byte(state);
+		printf("mov %s, byte %d \n", REGISTER_MODE_TABLE[w][reg], data);
+	}
 }
 
+void mov_m2a(struct sim_state_t *state, struct op_t *op) {
+	uint8_t w = state->op_byte & 1;
+	uint16_t addr = read_word(state);
 
-// ============================================================================
-// Operation list
-// ============================================================================
-const op_t OPS[] = {
-	(op_t) { .opcode_len = 6,  .opcode = 0b100010,   .impl = mov_rm2rm          },
-	(op_t) { .opcode_len = 7,  .opcode = 0b1100011,  .impl = mov_i2rm           },
-	(op_t) { .opcode_len = 4,  .opcode = 0b1011,     .impl = mov_i2r            },
-	(op_t) { .opcode_len = 7,  .opcode = 0b1010000,  .impl = mov_m2a            },
-	(op_t) { .opcode_len = 7,  .opcode = 0b1010001,  .impl = mov_a2m            },
-};
-const int NUM_OPS = sizeof(OPS)/sizeof(op_t);
+	if (w) {
+		printf("mov ax, [%d]\n", addr);
+	} else {
+		printf("mov al, [%d]\n", addr);
+	}
+}
+
+void mov_a2m(struct sim_state_t *state, struct op_t *op) {
+	uint8_t w = state->op_byte & 1;
+	uint16_t addr = read_word(state);
+
+	if (w) {
+		printf("mov [%d], ax\n", addr);
+	} else {
+		printf("mov [%d], al\n", addr);
+	}
+}
+
 
 // ============================================================================
 // Entrypoint
 // ============================================================================
 int main(int argc, char *argv[]) {
+	// Main-scoped constants
+	const struct op_t OPS[] = {
+		(struct op_t) { .opcode_len = 6,  .opcode = 34,  .impl = mov_rm2rm         },
+		(struct op_t) { .opcode_len = 7,  .opcode = 99,  .impl = mov_i2rm          },
+		(struct op_t) { .opcode_len = 4,  .opcode = 11,  .impl = mov_i2r           },
+		(struct op_t) { .opcode_len = 7,  .opcode = 80,  .impl = mov_m2a           },
+		(struct op_t) { .opcode_len = 7,  .opcode = 81,  .impl = mov_a2m           },
+	};
+	const int NUM_OPS = sizeof(OPS)/sizeof(struct op_t);
+
+	// "Parse" args
 	if (argc != 2) {
 		printf("Must provide machine code filename as sole argument\n");
 		return 1;
 	}
 
 	// Init state and open input file
-	sim_state_t state = {0};
+	struct sim_state_t state = {0};
 	state.fp = fopen(argv[1], "rb");
 	if (state.fp == NULL) {
 		printf("Could not open file {%s}\n", argv[1]);
@@ -202,13 +278,13 @@ int main(int argc, char *argv[]) {
 
 	// Read bytes while there are bytes left and we haven't hit an error
 	while (1) {
-		state.b0 = read_byte(state.fp);
+		state.op_byte = read_byte(&state);
 
 		// Check if current byte starts a known op
 		uint8_t found = 0;
 		for (int i = 0; i < NUM_OPS; i++) {
-			op_t op = OPS[i];
-			if ((state.b0 >> (8-op.opcode_len)) == op.opcode) {
+			struct op_t op = OPS[i];
+			if ((state.op_byte >> (8-op.opcode_len)) == op.opcode) {
 				op.impl(&state, &op);
 				found = 1;
 				break;
@@ -216,7 +292,7 @@ int main(int argc, char *argv[]) {
 		}
 
 		if (!found) {
-			printf("Unknown op from byte=%d\n", state.b0);
+			printf("Unknown op from byte=%d\n", state.op_byte);
 			fclose(state.fp);
 			return 100;
 		}
