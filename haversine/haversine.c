62d9a34eaa3bf06e4979dbdea03f20f47dff562a
Joseph Hines <ephjosdev@gmail.com>
2023-05-31 11:54:44 -0400

    Haversine parser

--- haversine/haversine.c
+++ haversine/haversine.c
@@ -1,3 +1,4 @@
+# include <assert.h>
 # include <ctype.h>
 # include <inttypes.h>
 # include <math.h>
@@ -33,6 +34,8 @@ enum token_type {
   TOKEN_COLON,
   TOKEN_IDENT,
   TOKEN_NUMBER,
+
+  TOKEN_UNKNOWN,
 };
 
 struct token {
@@ -40,6 +43,7 @@ struct token {
   union {
     char *ident;
     f64 number;
+    char unknown;
   };
 }; 
 
@@ -86,9 +90,79 @@ struct token *lex(char *filename) {
   char c;
 
   while((c = (char)fgetc(fp)) != EOF) {
-    printf("%c\n", c);
-
-    // TODO: get tokens
+    switch (c) {
+      case '{':
+        tokens[tokens_len++] = (struct token) {
+          .type = TOKEN_LSQUIRLY,
+        };
+        break;
+      case '}':
+        tokens[tokens_len++] = (struct token) {
+          .type = TOKEN_RSQUIRLY,
+        };
+        break;
+      case '[':
+        tokens[tokens_len++] = (struct token) {
+          .type = TOKEN_LBRACKET,
+        };
+        break;
+      case ']':
+        tokens[tokens_len++] = (struct token) {
+          .type = TOKEN_RBRACKET,
+        };
+        break;
+      case '"':
+        tokens[tokens_len++] = (struct token) {
+          .type = TOKEN_DQUOTE,
+        };
+        break;
+      case ',':
+        tokens[tokens_len++] = (struct token) {
+          .type = TOKEN_COMMA,
+        };
+        break;
+      case ':':
+        tokens[tokens_len++] = (struct token) {
+          .type = TOKEN_COLON,
+        };
+        break;
+      default:
+        {
+          if (isdigit(c) || c == '-') {
+            u32 buf_i = 0;
+            char *buf = calloc(sizeof(char), 128);
+            buf[buf_i++] = c;
+            while (isdigit((c = (char)fgetc(fp))) || c == '.') {
+              buf[buf_i++] = c;
+            }
+            fseek(fp, -1, SEEK_CUR);
+            tokens[tokens_len++] = (struct token) {
+              .type = TOKEN_NUMBER,
+              .number = atof(buf),
+            };
+            free(buf);
+          } else if (isalnum(c)) {
+            u32 buf_i = 0;
+            char *buf = calloc(sizeof(char), 128);
+            buf[buf_i++] = c;
+            while (isalnum((c = (char)fgetc(fp)))) {
+              buf[buf_i++] = c;
+            }
+            fseek(fp, -1, SEEK_CUR);
+            tokens[tokens_len++] = (struct token) {
+              .type = TOKEN_IDENT,
+              .ident = buf,
+            };
+          } else if (!isspace(c)) {
+            tokens[tokens_len++] = (struct token) {
+              .type = TOKEN_UNKNOWN,
+              .unknown = c,
+            };
+            break;
+          }
+        }
+        break;
+    }
 
     if (tokens_len >= tokens_cap) {
       tokens_cap <<= 1;
@@ -105,13 +179,141 @@ struct token *lex(char *filename) {
 }
 
 struct json_input parse(struct token *tokens) {
-  // TODO: parse tokens
-
-  return (struct json_input){
-    .pairs = NULL,
+  u32 pairs_cap = 1024;
+  struct json_input input = {
+    .pairs = malloc(sizeof(struct pair) * pairs_cap),
     .pairs_len = 0,
-    .expected = 1,
+    .expected = 0,
   };
+  
+#define STACK_SIZE 1024
+  u32 stack[STACK_SIZE] = {0};
+  u32 sp = 0;
+
+  u32 i = 0;
+  struct token curr = tokens[i++];
+  while (curr.type != TOKEN_END) {
+    switch (curr.type) {
+      case TOKEN_LSQUIRLY:
+      case TOKEN_LBRACKET:
+      case TOKEN_DQUOTE:
+        if (sp && stack[sp-1] == TOKEN_DQUOTE) {
+          assert(stack[--sp] == TOKEN_DQUOTE);
+        } else {
+          stack[sp++] = curr.type;
+        }
+        break;
+      case TOKEN_RSQUIRLY:
+        assert(stack[--sp] == TOKEN_LSQUIRLY);
+        break;
+      case TOKEN_RBRACKET:
+        assert(stack[--sp] == TOKEN_LBRACKET);
+        break;
+      case TOKEN_IDENT:
+        if (sp == 2) {
+          if (strcmp(curr.ident, "expected") == 0) {
+            assert((curr = tokens[i++]).type == TOKEN_DQUOTE);
+            assert(stack[--sp] == TOKEN_DQUOTE);
+            assert((curr = tokens[i++]).type == TOKEN_COLON);
+            assert((curr = tokens[i++]).type == TOKEN_NUMBER);
+            input.expected = curr.number;
+          } else {
+            assert(strcmp(curr.ident, "pairs") == 0);
+          }
+        } else if (sp == 4) {
+          // TODO: get pair
+          struct pair p = {0};
+
+          char *curr_ident = curr.ident;
+          assert((curr = tokens[i++]).type == TOKEN_DQUOTE);
+          assert(stack[--sp] == TOKEN_DQUOTE);
+          assert((curr = tokens[i++]).type == TOKEN_COLON);
+          assert((curr = tokens[i++]).type == TOKEN_NUMBER);
+          if (strcmp(curr_ident, "x0") == 0) {
+            p.x0 = curr.number;
+          } else if (strcmp(curr_ident, "x1") == 0) {
+            p.x1 = curr.number;
+          } else if (strcmp(curr_ident, "y0") == 0) {
+            p.y0 = curr.number;
+          } else if (strcmp(curr_ident, "y1") == 0) {
+            p.y1 = curr.number;
+          }
+          assert((curr = tokens[i++]).type == TOKEN_COMMA);
+          assert((curr = tokens[i++]).type == TOKEN_DQUOTE);
+          stack[sp++] = curr.type;
+          assert((curr = tokens[i++]).type == TOKEN_IDENT);
+
+          curr_ident = curr.ident;
+          assert((curr = tokens[i++]).type == TOKEN_DQUOTE);
+          assert(stack[--sp] == TOKEN_DQUOTE);
+          assert((curr = tokens[i++]).type == TOKEN_COLON);
+          assert((curr = tokens[i++]).type == TOKEN_NUMBER);
+          if (strcmp(curr_ident, "x0") == 0) {
+            p.x0 = curr.number;
+          } else if (strcmp(curr_ident, "x1") == 0) {
+            p.x1 = curr.number;
+          } else if (strcmp(curr_ident, "y0") == 0) {
+            p.y0 = curr.number;
+          } else if (strcmp(curr_ident, "y1") == 0) {
+            p.y1 = curr.number;
+          }
+          assert((curr = tokens[i++]).type == TOKEN_COMMA);
+          assert((curr = tokens[i++]).type == TOKEN_DQUOTE);
+          stack[sp++] = curr.type;
+          assert((curr = tokens[i++]).type == TOKEN_IDENT);
+
+          curr_ident = curr.ident;
+          assert((curr = tokens[i++]).type == TOKEN_DQUOTE);
+          assert(stack[--sp] == TOKEN_DQUOTE);
+          assert((curr = tokens[i++]).type == TOKEN_COLON);
+          assert((curr = tokens[i++]).type == TOKEN_NUMBER);
+          if (strcmp(curr_ident, "x0") == 0) {
+            p.x0 = curr.number;
+          } else if (strcmp(curr_ident, "x1") == 0) {
+            p.x1 = curr.number;
+          } else if (strcmp(curr_ident, "y0") == 0) {
+            p.y0 = curr.number;
+          } else if (strcmp(curr_ident, "y1") == 0) {
+            p.y1 = curr.number;
+          }
+          assert((curr = tokens[i++]).type == TOKEN_COMMA);
+          assert((curr = tokens[i++]).type == TOKEN_DQUOTE);
+          stack[sp++] = curr.type;
+          assert((curr = tokens[i++]).type == TOKEN_IDENT);
+
+          curr_ident = curr.ident;
+          assert((curr = tokens[i++]).type == TOKEN_DQUOTE);
+          assert(stack[--sp] == TOKEN_DQUOTE);
+          assert((curr = tokens[i++]).type == TOKEN_COLON);
+          assert((curr = tokens[i++]).type == TOKEN_NUMBER);
+          if (strcmp(curr_ident, "x0") == 0) {
+            p.x0 = curr.number;
+          } else if (strcmp(curr_ident, "x1") == 0) {
+            p.x1 = curr.number;
+          } else if (strcmp(curr_ident, "y0") == 0) {
+            p.y0 = curr.number;
+          } else if (strcmp(curr_ident, "y1") == 0) {
+            p.y1 = curr.number;
+          }
+
+          input.pairs[input.pairs_len++] = p;
+
+          if (input.pairs_len >= pairs_cap) {
+            pairs_cap <<= 1;
+            input.pairs = realloc(input.pairs, sizeof(struct pair) * pairs_cap);
+          }
+        }
+        break;
+      default:
+        break;
+    }
+
+    curr = tokens[i++];
+  }
+
+  assert(sp == 0);
+
+  return input;
 }
 
 int main(int argc, char *argv[]) {
@@ -130,10 +332,20 @@ int main(int argc, char *argv[]) {
 
   f64 average = (f64)sum/input.pairs_len;
 
-  printf("expected = %12.4f\nactual   = %12.4f\n", input.expected, average);
+  printf("expected = %12.6f\nactual   = %12.6f\n", input.expected, average);
   
   if (input.pairs != NULL) free(input.pairs);
-  if (tokens != NULL) free(tokens);
+  if (tokens != NULL) {
+    u32 i = 0;
+    struct token curr = tokens[i++];
+    while (curr.type != TOKEN_END) {
+      if (curr.type == TOKEN_IDENT) {
+        free(curr.ident);
+      }
+      curr = tokens[i++];
+    }
+    free(tokens);
+  }
 
   return 0;
 }
